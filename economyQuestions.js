b386dc3b4d650a7dc9e7730a79ed746284d72435
cpsh1234 <58405996+cpsh1234@users.noreply.github.com>
2020-01-09 13:03:24 +0000

    Create economyQuestions.js

---
+++ economyQuestions.js
@@ -0,0 +1,172 @@
+economyQuestions = [
+{
+        "question": "The UK should nationalise Water.",
+        "effect": {
+            "lab": 2,
+            "con": -2,
+            "ld": 0,
+            "lib": -2,
+            "cit": -2
+        }
+    },
+    {
+        "question": "The UK should nationalise Royal Mail.",
+        "effect": {
+            "lab": 2,
+            "con": -2,
+            "ld": 1,
+            "lib": -2,
+            "cit": 0
+        }
+    },
+    {
+        "question": "The UK should provide free broadband for all.",
+        "effect": {
+            "lab": 2,
+            "con": -2,
+            "ld": -1,
+            "lib": -2,
+            "cit": -2
+        }
+    },
+    {
+        "question": "Tax should be replaced by a single flat tax on income.",
+        "effect": {
+            "lab": -2,
+            "con": 1,
+            "ld": -2,
+            "lib": 2,
+            "cit": 1
+        }
+    },
+    {
+        "question": "Business rates should be reduced.",
+        "effect": {
+            "lab": -1,
+            "con": 2,
+            "ld": 1,
+            "lib": 2,
+            "cit": 1
+        }
+    },
+    {
+        "question": "There should be a tax free allowance on corporation tax.",
+        "effect": {
+            "lab": -2,
+            "con": 1,
+            "ld": -1,
+            "lib": 2,
+            "cit": 1
+        }
+    },
+    {
+        "question": "The TV licence should be abolished.",
+        "effect": {
+            "lab": -2,
+            "con": 2,
+            "ld": -1,
+            "lib": 2,
+            "cit": -1
+        }
+    },
+    {
+        "question": "Inheritance tax should be abolished.",
+        "effect": {
+            "lab": -2,
+            "con": 1,
+            "ld": -1,
+            "lib": 2,
+            "cit": -2
+        }
+    },
+    {
+        "question": "The state should, no matter what, always aim to achieve a tax surplus.",
+        "effect": {
+            "lab": -2,
+            "con": 2,
+            "ld": -2,
+            "lib": 2,
+            "cit": -1
+        }
+    },
+    {
+        "question": "The rich are too highly taxed.",
+        "effect": {
+            "lab": -2,
+            "con": 2,
+            "ld": -2,
+            "lib": 2,
+            "cit": 0
+        }
+    },
+    {
+        "question": "All sin taxes should be abolished.",
+        "effect": {
+            "lab": -2,
+            "con": 2,
+            "ld": -1,
+            "lib": 2,
+            "cit": -2
+        }
+    },
+    {
+        "question": "There should be a tax on abandoned homes.",
+        "effect": {
+            "lab": 1,
+            "con": 2,
+            "ld": 2,
+            "lib": -1,
+            "cit": 1
+        }
+    },
+    {
+        "question": "VAT on energy bills should be scrapped.",
+        "effect": {
+            "lab": -2,
+            "con": 2,
+            "ld": -1,
+            "lib": 2,
+            "cit": 1
+        }
+    },
+    {
+        "question": "Income tax should be increased by 1% to fund the NHS.",
+        "effect": {
+            "lab": 2,
+            "con": 0,
+            "ld": 2,
+            "lib": -2,
+            "cit": -1
+        }
+    },
+    {
+        "question": "There should be a tax on frequent fliers.",
+        "effect": {
+            "lab": 1,
+            "con": -2,
+            "ld": 2,
+            "lib": -2,
+            "cit": -1
+        }
+    },
+    {
+        "question": "All public services, other than the police and army, should be franchised out to private companies.",
+        "effect": {
+            "lab": -2,
+            "con": 1,
+            "ld": -2,
+            "lib": 1,
+            "cit": 2
+        }
+    },
+    {
+        "question": "Tax brackets should be replaced by a smoother tax system.",
+        "effect": {
+            "lab": -1,
+            "con": 2,
+            "ld": -1,
+            "lib": 1,
+            "cit": 1
+        }
+    }
+    ];
