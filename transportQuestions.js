c02279e40d3fbb1f93941981e5aef492fab12f54
cpsh1234 <58405996+cpsh1234@users.noreply.github.com>
2020-01-09 13:04:29 +0000

    Create transportQuestions.js

---
+++ transportQuestions.js
@@ -0,0 +1,62 @@
+transportQuestions = [
+{
+        "question": "The train lines should be nationalised.",
+        "effect": {
+            "lab": 2,
+            "con": -2,
+            "ld": 1,
+            "lib": -2,
+            "cit": -1
+        }
+    },
+    {
+        "question": "HS2 should be scrapped.",
+        "effect": {
+            "lab": 1,
+            "con": -2,
+            "ld": 2,
+            "lib": 2,
+            "cit": -2
+        }
+    },
+    {
+        "question": "UK airports should be able to build new runways.",
+        "effect": {
+            "lab": -1,
+            "con": 2,
+            "ld": 1,
+            "lib": 2,
+            "cit": 1
+        }
+    },
+    {
+        "question": "Train lines should be electrified.",
+        "effect": {
+            "lab": -1,
+            "con": 2,
+            "ld": 1,
+            "lib": -1,
+            "cit": 1
+        }
+    },
+    {
+        "question": "Roads should be privatised to reduce car tax.",
+        "effect": {
+            "lab": -2,
+            "con": 2,
+            "ld": -2,
+            "lib": 1,
+            "cit": 0
+        }
+    },
+    {
+        "question": "Rail fares should be frozen for the next five years.",
+        "effect": {
+            "lab": 2,
+            "con": -1,
+            "ld": 2,
+            "lib": -2,
+            "cit": 1
+        }
+    }
+    ];
