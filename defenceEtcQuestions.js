060ea1d5772e66d9e0fe46f4963b2d7b729ad775
cpsh1234 <58405996+cpsh1234@users.noreply.github.com>
2020-01-09 13:05:19 +0000

    Create defenceEtcQuestions.js

---
+++ defenceEtcQuestions.js
@@ -0,0 +1,132 @@
+defenceEtcQuestions = [
+{
+        "question": "The UK should recognise Palestine as a separate nation.",
+        "effect": {
+            "lab": 2,
+            "con": -2,
+            "ld": 0,
+            "lib": -1,
+            "cit": 1
+        }
+    },
+    {
+        "question": "The UK should leave the EU.",
+        "effect": {
+            "lab": -1,
+            "con": 2,
+            "ld": -2,
+            "lib": 2,
+            "cit": 0
+        }
+    },
+    {
+        "question": "The UK should commit at least 0.7% of GDP to foreign aid.",
+        "effect": {
+            "lab": 2,
+            "con": -2,
+            "ld": 2,
+            "lib": -2,
+            "cit": 0
+        }
+    },
+    {
+        "question": "The UK should not give foreign aid to any country that has a space program.",
+        "effect": {
+            "lab": -2,
+            "con": 2,
+            "ld": 2,
+            "lib": 2,
+            "cit": 1
+        }
+    },
+    {
+        "question": "The UK should not give foreign aid to any country that has a nuclear program.",
+        "effect": {
+            "lab": -2,
+            "con": 2,
+            "ld": 2,
+            "lib": 2,
+            "cit": 1
+        }
+    },
+    {
+        "question": "The UK should try to impose our values on the rest of the world.",
+        "effect": {
+            "lab": -2,
+            "con": 2,
+            "ld": 1,
+            "lib": 1,
+            "cit": -1
+        }
+    },
+    {
+        "question": "By 2022 the UK should accept no more than 1000 asylum seekers per year.",
+        "effect": {
+            "lab": -2,
+            "con": 2,
+            "ld": -2,
+            "lib": 1,
+            "cit": -1
+        }
+    },
+    {
+        "question": "All foreign nationals who aid the armed forces should receive British citizenship.",
+        "effect": {
+            "lab": 1,
+            "con": 2,
+            "ld": -1,
+            "lib": -1,
+            "cit": 1
+        }
+    },
+    {
+        "question": "The UK should have a points-based immigration system.",
+        "effect": {
+            "lab": -2,
+            "con": 2,
+            "ld": -1,
+            "lib": 1,
+            "cit": -1
+        }
+    },
+    {
+        "question": "Trident should be scrapped.",
+        "effect": {
+            "lab": 2,
+            "con": -2,
+            "ld": -1,
+            "lib": -2,
+            "cit": -2
+        }
+    },
+    {
+        "question": "The UK should stop selling weapons to Saudi Arabia.",
+        "effect": {
+            "lab": 2,
+            "con": -2,
+            "ld": 2,
+            "lib": -2,
+            "cit": 0
+        }
+    },
+    {
+        "question": "Military service in the Armed forces should be compulsory.",
+        "effect": {
+            "lab": -2,
+            "con": 2,
+            "ld": -2,
+            "lib": -1,
+            "cit": -2
+        }
+    },
+    {
+        "question": "The UK should be able to assassinate any perceived threat to national security without trial.",
+        "effect": {
+            "lab": -2,
+            "con": 2,
+            "ld": -2,
+            "lib": 1,
+            "cit": -2
+        }
+    }
+];
