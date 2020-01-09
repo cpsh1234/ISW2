91fac89a5ba09e4701242aaa14a6b297795b713e
cpsh1234 <58405996+cpsh1234@users.noreply.github.com>
2020-01-09 12:58:53 +0000

    Create educationQuestions.js

---
+++ educationQuestions.js
@@ -0,0 +1,82 @@
+educationQuestions = [
+{
+        "question": "All schools should be funded equally.",
+        "effect": {
+            "lab": 2,
+            "con": -1,
+            "ld": 1,
+            "lib": -2,
+            "cit": -1
+        }
+    },
+    {
+        "question": "The SATs tests should be abolished.",
+        "effect": {
+            "lab": 2,
+            "con": -1,
+            "ld": 1,
+            "lib": 1,
+            "cit": -1
+        }
+    },
+    {
+        "question": "University should be free for all people.",
+        "effect": {
+            "lab": 2,
+            "con": -2,
+            "ld": -1,
+            "lib": -2,
+            "cit": -2
+        }
+    },
+    {
+        "question": "The best universities should be allowed to charge unlimited fees.",
+        "effect": {
+            "lab": -2,
+            "con": 2,
+            "ld": -1,
+            "lib": 1,
+            "cit": -2
+        }
+    },
+    {
+        "question": "All children should be expected to learn british sign language.",
+        "effect": {
+            "lab": 1,
+            "con": 1,
+            "ld": 2,
+            "lib": -2,
+            "cit": -2
+        }
+    },
+    {
+        "question": "All families should be given an educational voucher to make private education more accessible to all.",
+        "effect": {
+            "lab": -2,
+            "con": 1,
+            "ld": -1,
+            "lib": 2,
+            "cit": 1
+        }
+    },
+    {
+        "question": "All children should be expected to learn to code a computer.",
+        "effect": {
+            "lab": -1,
+            "con": 1,
+            "ld": 2,
+            "lib": 1,
+            "cit": 1
+        }
+    },
+    {
+        "question": "The current education system is too narrow and should be made broader and less restrictive.",
+        "effect": {
+            "lab": -1,
+            "con": -1,
+            "ld": 1,
+            "lib": 2,
+            "cit": 1
+        }
+    }
+    ];
