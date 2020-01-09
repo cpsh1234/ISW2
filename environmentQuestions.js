2d35666f0277e2a3092e555a95bd11fd8939aa56
cpsh1234 <58405996+cpsh1234@users.noreply.github.com>
2020-01-09 13:02:31 +0000

    Create environmentQuestions.js

---
+++ environmentQuestions.js
@@ -0,0 +1,132 @@
+environmentQuestions = [
+{
+        "question": "The UK should subsidise replacements of fossil fuels.",
+        "effect": {
+            "lab": 2,
+            "con": -1,
+            "ld": 2,
+            "lib": -2,
+            "cit": 2
+        }
+    },
+    {
+        "question": "The UK should immediately stop exporting its waste to other countries.",
+        "effect": {
+            "lab": -2,
+            "con": -1,
+            "ld": -2,
+            "lib": 2,
+            "cit": -1
+        }
+    },
+    {
+        "question": "The UK should be Carbon-Neutral by 2030.",
+        "effect": {
+            "lab": 1,
+            "con": -2,
+            "ld": 2,
+            "lib": -2,
+            "cit": -1
+        }
+    },
+    {
+        "question": "The UK should be Carbon-Neutral by 2050.",
+        "effect": {
+            "lab": 2,
+            "con": 2,
+            "ld": 2,
+            "lib": -1,
+            "cit": 2
+        }
+    },
+    {
+        "question": "All pesticides that might harm bees should be banned.",
+        "effect": {
+            "lab": 1,
+            "con": 2,
+            "ld": 1,
+            "lib": -2,
+            "cit": 2
+        }
+    },
+    {
+        "question": "There should be a tax on single use coffee cups.",
+        "effect": {
+            "lab": 1,
+            "con": 2,
+            "ld": 2,
+            "lib": -2,
+            "cit": 1
+        }
+    },
+    {
+        "question": "All new houses should have smart meters automatically installed.",
+        "effect": {
+            "lab": -1,
+            "con": 2,
+            "ld": -1,
+            "lib": 1,
+            "cit": -1
+        }
+    },
+    {
+        "question": "The UK should exploit geothermal power.",
+        "effect": {
+            "lab": 1,
+            "con": 2,
+            "ld": 1,
+            "lib": -2,
+            "cit": 1
+        }
+    },
+    {
+        "question": "All overhead electric cables should be buried.",
+        "effect": {
+            "lab": -1,
+            "con": 2,
+            "ld": -2,
+            "lib": -1,
+            "cit": -2
+        }
+    },
+    {
+        "question": "The UK should declare a climate emergency.",
+        "effect": {
+            "lab": 2,
+            "con": -1,
+            "ld": 2,
+            "lib": -1,
+            "cit": -2
+        }
+    },
+    {
+        "question": "It should be illegal to raise caged hens.",
+        "effect": {
+            "lab": 1,
+            "con": -1,
+            "ld": 2,
+            "lib": -2,
+            "cit": 1
+        }
+    },
+    {
+        "question": "The UK should support fracking.",
+        "effect": {
+            "lab": -2,
+            "con": 2,
+            "ld": -2,
+            "lib": 1,
+            "cit": -2
+        }
+    },
+    {
+        "question": "The UK government should invest more into nuclear power.",
+        "effect": {
+            "lab": -1,
+            "con": 1,
+            "ld": 1,
+            "lib": -1,
+            "cit": 2
+        }
+    }
+    ];
