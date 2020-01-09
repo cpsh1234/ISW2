eb4eef81fe7ef4a47a8e156eb64a659c4c7f7c0a
cpsh1234 <58405996+cpsh1234@users.noreply.github.com>
2020-01-09 13:01:23 +0000

    Create constitutionQuestions.js

---
+++ constitutionQuestions.js
@@ -0,0 +1,92 @@
+constitutionQuestions = [
+{
+        "question": "16-year olds should be allowed to vote.",
+        "effect": {
+            "lab": 2,
+            "con": -2,
+            "ld": 2,
+            "lib": -2,
+            "cit": -2
+        }
+    },
+    {
+        "question": "The courts of the land need to be streamlined and simplified.",
+        "effect": {
+            "lab": 1,
+            "con": 1,
+            "ld": -1,
+            "lib": 1,
+            "cit": 2
+        }
+    },
+    {
+        "question": "The monarchy should be abolished.",
+        "effect": {
+            "lab": 2,
+            "con": -2,
+            "ld": 0,
+            "lib": -2,
+            "cit": -2
+        }
+    },
+    {
+        "question": "Only English MPs should be allowed to vote on laws that affect England only.",
+        "effect": {
+            "lab": -1,
+            "con": 2,
+            "ld": -1,
+            "lib": 1,
+            "cit": 1
+        }
+    },
+    {
+        "question": "The United Kingdom should adopt federalism.",
+        "effect": {
+            "lab": 2,
+            "con": -2,
+            "ld": 1,
+            "lib": -2,
+            "cit": 0
+        }
+    },
+    {
+        "question": "The House of Lords should be abolished.",
+        "effect": {
+            "lab": 2,
+            "con": -1,
+            "ld": 2,
+            "lib": -1,
+            "cit": -2
+        }
+    },
+    {
+        "question": "Corporations and Unions should be allowed to donate money to political parties.",
+        "effect": {
+            "lab": 2,
+            "con": 1,
+            "ld": -1,
+            "lib": 2,
+            "cit": 1
+        }
+    },
+    {
+        "question": "Convicted criminals should be afforded the right to vote.",
+        "effect": {
+            "lab": 2,
+            "con": -1,
+            "ld": 1,
+            "lib": -2,
+            "cit": 1
+        }
+    },
+    {
+        "question": "Convicted criminals should be afforded the right to stand for election.",
+        "effect": {
+            "lab": 2,
+            "con": -1,
+            "ld": 1,
+            "lib": -2,
+            "cit": 1
+        }
+    }
+    ];
