3a0962f26638afdc8b02ede2ac8804e2b22cfc99
cpsh1234 <58405996+cpsh1234@users.noreply.github.com>
2020-01-09 12:57:44 +0000

    Create healthcareQuestions.js

---
+++ healthcareQuestions.js
@@ -0,0 +1,91 @@
+healthcareQuestions = [
+        "question": "The NHS needs to have higher real-terms funding.",
+        "effect": {
+            "lab": 2,
+            "con": 0,
+            "ld": 2,
+            "lib": -2,
+            "cit": -1
+        }
+    },
+    {
+        "question": "All prescriptions should be free on the NHS, no matter your income.",
+        "effect": {
+            "lab": 2,
+            "con": -2,
+            "ld": 0,
+            "lib": -2,
+            "cit": -2
+        }
+    },
+    {
+        "question": "All dental work should be free, no matter your income.",
+        "effect": {
+            "lab": 2,
+            "con": -2,
+            "ld": -1,
+            "lib": -2,
+            "cit": -1
+        }
+    },
+    {
+        "question": "It is immoral for any private company to profit from the NHS.",
+        "effect": {
+            "lab": 2,
+            "con": -1,
+            "ld": 1,
+            "lib": -2,
+            "cit": -1
+        }
+    },
+    {
+        "question": "It is unfair for people who pay for private health insurance to also pay for the NHS.",
+        "effect": {
+            "lab": -2,
+            "con": 0,
+            "ld": -2,
+            "lib": 2,
+            "cit": -1
+        }
+    },
+    {
+        "question": "No one but UK nationals should get free treatment from the NHS, no matter their condition.",
+        "effect": {
+            "lab": -2,
+            "con": 1,
+            "ld": -1,
+            "lib": 2,
+            "cit": -1
+        }
+    },
+    {
+        "question": "The NHS should be abolished.",
+        "effect": {
+            "lab": -2,
+            "con": -1,
+            "ld": -2,
+            "lib": 2,
+            "cit": -1
+        }
+    },
+    {
+        "question": "Doctors deserve to be paid more.",
+        "effect": {
+            "lab": 2,
+            "con": 0,
+            "ld": 2,
+            "lib": -1,
+            "cit": -1
+        }
+    },
+    {
+        "question": "It is right that as prescription drug prices increase that burden should be passed on to those who use the drugs.",
+        "effect": {
+            "lab": -2,
+            "con": 2,
+            "ld": -1,
+            "lib": 1,
+            "cit": 1
+        }
+    }
+    ];
