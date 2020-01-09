20b079e6c7060d837404694ff0cc363568f04eec
cpsh1234 <58405996+cpsh1234@users.noreply.github.com>
2020-01-09 13:00:28 +0000

    Create socialQuestions.js

---
+++ socialQuestions.js
@@ -0,0 +1,112 @@
+socialQuestions = [
+{
+        "question": "Murderers who don't disclose the location of bodies should be denied parole.",
+        "effect": {
+            "lab": -1,
+            "con": 1,
+            "ld": 1,
+            "lib": 1,
+            "cit": -1
+        }
+    },
+    {
+        "question": "Free speech should be guaranteed and that includes hate speech and incitement of violence.",
+        "effect": {
+            "lab": -2,
+            "con": 0,
+            "ld": -1,
+            "lib": 2,
+            "cit": -2
+        }
+    },
+    {
+        "question": "All people, subject to minimal checks, should be allowed to carry firearms.",
+        "effect": {
+            "lab": -1,
+            "con": -1,
+            "ld": -2,
+            "lib": 2,
+            "cit": -2
+        }
+    },
+    {
+        "question": "The Human Rights Act should be repealed.",
+        "effect": {
+            "lab": -2,
+            "con": 2,
+            "ld": -2,
+            "lib": 2,
+            "cit": -2
+        }
+    },
+    {
+        "question": "There should be more police officers on the streets.",
+        "effect": {
+            "lab": 2,
+            "con": 2,
+            "ld": 1,
+            "lib": -1,
+            "cit": 2
+        }
+    },
+    {
+        "question": "Terrorists get off too easily.",
+        "effect": {
+            "lab": -1,
+            "con": 2,
+            "ld": 1,
+            "lib": 2,
+            "cit": -2
+        }
+    },
+    {
+        "question": "Rough sleepers should be forced to live in homeless shelters and sleeping on the streets should be illegal.",
+        "effect": {
+            "lab": -2,
+            "con": 2,
+            "ld": 1,
+            "lib": 1,
+            "cit": -2
+        }
+    },
+    {
+        "question": "Recreational drug use should be decriminalised.",
+        "effect": {
+            "lab": 2,
+            "con": -1,
+            "ld": 1,
+            "lib": 2,
+            "cit": 2
+        }
+    },
+    {
+        "question": "People should be allowed to get divorced without finding fault in the other person.",
+        "effect": {
+            "lab": 2,
+            "con": -1,
+            "ld": 1,
+            "lib": -1,
+            "cit": 2
+        }
+    },
+    {
+        "question": "The law should reflect that both a man and a woman can be raped.",
+        "effect": {
+            "lab": 2,
+            "con": 2,
+            "ld": 1,
+            "lib": 2,
+            "cit": 2
+        }
+    },
+    {
+        "question": "The death penalty should be an option for the most serious of crimes.",
+        "effect": {
+            "lab": -1,
+            "con": 1,
+            "ld": -1,
+            "lib": 2,
+            "cit": -2
+        }
+    }
+    ];
